d4bbc03d07e25a786b35de8153c5ea1c76fd40ec
agez0s <eduardo.cordeiro@protonmail.com>
2021-11-22 20:54:44 -0300

    Variáveis de ambiente para DB

--- conn/conn.js
+++ conn/conn.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
-const Conn = () => {
+const Conn = (url, user, pass, banco) => {
   mongoose
-    .connect("mongodb://localhost:27017/todos", {
+    .connect(`${url}/${banco}`, {
+      user: user,
+      pass: pass,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require("express");
 const cors = require("cors");
 const Conn = require("./conn/conn");
@@ -11,7 +15,12 @@ app.use(cors());
 
 app.use("/", todoRouter);
 
-Conn();
-app.listen(port, () => {
+const DB_URL = process.env.DB_URL
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_DATA = process.env.DB_DATA
+
+Conn(DB_URL, DB_USER, DB_PASS, DB_DATA);
+app.listen(process.env.PORT || port, () => {
   console.log(`Rodando servidor em http://localhost:${port}`);
 });
